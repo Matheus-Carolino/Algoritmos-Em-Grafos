a1f811b62448f2a38c4b8507522be1942c678192
Matheus-Carolino <matheussilvacarolino@gmail.com>
2026-07-18 17:12:47 -0300

    add/structs e leitura de arquivos

---
+++ include/validacao.h
@@ -0,0 +1,15 @@
+#ifndef GRAFOS_VALIDACAO_H
+#define GRAFOS_VALIDACAO_H
+    //typedef para o enum de validacao
+    typedef enum {
+        GRAFO_OK,
+        ERRO_ARQUIVO_NAO_ENCONTRADO,
+        ERRO_FORMATO_INVALIDO,
+        ERRO_NUMERO_INVALIDO,
+        ERRO_ARESTA_INVALIDA,
+        ERRO_MEMORIA_INSUFICIENTE
+    } GrafoStatus;
+
+void imprimirMensagemGrafo(GrafoStatus status);
+
+#endif //GRAFOS_VALIDACAO_H
--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+#include "validacao.h"
+#include "grafo.h"
+
 int main(void) {
-    printf("Hello, World!\n");
+    printf("Teste de Leitura do arquivo grafo\n");
+    Grafo *grafo;
+    GrafoStatus status;
+
+    status = lerGrafoDeArquivo("../data/grafo.txt", &grafo);
+    imprimirMensagemGrafo(status);
+
+    liberarGrafo(grafo);
     return 0;
 }
